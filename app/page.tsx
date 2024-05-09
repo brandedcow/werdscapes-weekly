cc26e7d81e82d4ac3592d5b2afbe3c677c05cef9
Chris Kang <chris.v.kang@gmail.com>
2024-05-09 17:53:23 -0500

    feat: Added shadcn components to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Wordscapes Weekly</h1>
+      <div>
+        <h2>Enter player name to track tags</h2>
+        <Input />
+        <Button>Submit</Button>
+      </div>
+    </main>
   );
 }
