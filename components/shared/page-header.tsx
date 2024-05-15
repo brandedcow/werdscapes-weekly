b47b1c8edfabd8dc9304a54f7d457c49af3e1a5f
Chris Kang <chris.v.kang@gmail.com>
2024-05-15 17:03:20 -0500

    feat: add link to page header

--- components/shared/page-header.tsx
+++ components/shared/page-header.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import { DarkModeToggle } from "./dark-mode-toggle";
 
 export function PageHeader() {
   return (
     <div className="w-full flex justify-between items-center">
-      <h1 className="text-2xl">🏆 Wordscapes Weekly</h1>
+      <Link href="/dashboard">
+        <h1 className="text-2xl">🏆 Wordscapes Weekly</h1>
+      </Link>
       <DarkModeToggle />
     </div>
   );
